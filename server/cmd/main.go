d476f809fb9478f4781d798df5f402a842b1cae1
Matthew Yu <myu2@umbc.edu>
2024-12-01 23:43:53 -0500

    feat: played around w/ HTTP server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"notepad/internal/config"
 	"notepad/internal/server"
+	"os"
 )
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
 
 func main() {
 	// Load configuration
@@ -16,5 +31,17 @@ func main() {
 	// Start the server
 	if err := server.Start(cfg); err != nil { // Assuming server.Start returns an error
 		log.Fatalf("Error starting server: %v", err)
+	}
+
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err = http.ListenAndServe("127.0.0.1:3000", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server  closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
 	}
 }
