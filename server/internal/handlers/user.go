baa2af4eae046ce9563132b2a35e6d8f94a6fe9d
Matthew Yu <myu2@umbc.edu>
2024-12-03 23:30:59 -0500

    feat: first iteration of create user

--- server/internal/handlers/user.go
+++ server/internal/handlers/user.go
@@ -5,12 +5,24 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// Set up ID and User store
+var nextID = 1
+var users = []User{}
 
 func RegisterUserRoutes() {
 	http.HandleFunc("GET /users", GetUsers)
 	http.HandleFunc("GET /users/{id}", GetUserByID)
+	http.HandleFunc("POST /users", CreateUser)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +32,37 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 func GetUserByID(w http.ResponseWriter, r *http.Request) {
 	idString := r.PathValue("id")
 	w.Write([]byte("User ID: " + idString))
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var newUser User
+
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	if newUser.Name == "" || newUser.Email == "" {
+		http.Error(w, "Name and Email are required", http.StatusBadRequest)
+		return
+	}
+
+	// Assign new ID & Increment ID for next user
+	newUser.ID = nextID
+	nextID++
+
+	users = append(users, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
 }
